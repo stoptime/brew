f78a0bf3724a680b80c5c52bedbf091414ab41a6
Matt Mayes Lap <matt@stoptimestudio.com>
2012-01-14 13:20:08 -0600

    HTML Validation

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $recipes = array();
-$dir = './_recipes';
+$dir = './_html';
 
 if ($handle = opendir($dir)) {
 	while (false !== ($entry = readdir($handle))) {
@@ -23,22 +23,69 @@ if ($handle = opendir($dir)) {
 }
 
 //var_dump($recipes);
+$content = '';
 
 foreach ($recipes as $key => $value) {
-	echo '<ul>';
-	printf('<li><b><a href="%s/%s.html">%s</a></b>', $dir, $key, $key);
+	$content .= '<ul>';
+	$content .= sprintf('<li><b><a href="%s/%s.html">%s</a></b>', $dir, str_replace(' ', '%20', $key), $key);
 	if (isset($value)) {
-		echo '<ul>';
+		$content .=  '<ul>';
 		foreach ($value as $k => $v) {
 			// we only want the batch
+			$v = trim($v);
 			$n = explode('-', $v);
 			$l = count($n);
 			// strip the html
 			$final_name = explode('.', $n[$l-1]);
-			printf('<li><a href="%s/%s">%s</a></li>', $dir, $v, $final_name[0]);
+			$content .= sprintf('<li><a href="%s/%s">%s</a></li>', $dir, str_replace(' ', '%20', $v), $final_name[0]);
 		}
-		echo '</ul>';
+		$content .= '</ul>';
 	}
-	echo "</li>";
-	echo '</ul>';
+	$content .= "</li>";
+	$content .= '</ul>';
 }
+?>
+
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Matt's Recipes</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<style type="text/css" media="screen">
+			body {
+				font-family: helvetica;
+				color: #aaa;
+				font-size: 85%;
+				padding-bottom: 50px;
+			}
+			h1 {
+				font-size: 140%;
+				border-bottom: 1px dashed #ccc;
+			}
+			a {
+				color: green;
+				display: block;
+				padding-bottom: 5px;
+			}
+			a:hover {
+				text-decoration: none;
+				color: blue;
+				background: #FF9;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Matt's Homebrew Recipes</h1>
+		<?=$content?>
+	</body>
+</html>
+
+
+
+	
+	
+	
+	
+	
+	
