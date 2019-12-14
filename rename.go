09c37fdff5e2b2c258f12c4ead19a719284334fd
Matt Mayes <mattamayes@gmail.com>
2019-12-14 17:09:20 -0600

    IPA upates and golang rename script.

---
+++ rename.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/kennygrant/sanitize"
+)
+
+var (
+	fileInfo  os.FileInfo
+	err       error
+	directory = "./_html/"
+	ext       string
+	fileBase  string
+	saniFile  string
+)
+
+func main() {
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		ext = path.Ext(file.Name())
+		fileBase = strings.TrimSuffix(file.Name(), ext)
+		fileBase = strings.TrimSuffix(fileBase, "-")
+		saniFile = sanitize.BaseName(fileBase) + ext
+		err := os.Rename(directory+file.Name(), directory+saniFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
